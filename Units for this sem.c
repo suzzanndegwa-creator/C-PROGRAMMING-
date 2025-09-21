d563a29d2d171572fc825ea20d07c4f5b5105358
suzzanndegwa-creator <suzzanndegwa@gmail.com>
2025-09-21 17:19:35 +0300

    Add files via upload

---
+++ Units for this sem.c
@@ -0,0 +1,22 @@
+//simple c program
+/*
+Name:Susan Ndegwa
+Reg No:CT101/G/26435/25
+Description:units taken
+*/
+
+
+#include<stdio.h>
+//main function
+int main(){
+printf("Mathematics for science\n");
+printf("Physics\n");
+printf("Computer architecture\n");
+printf("Programming\n");
+printf("Discrete mathematics\n");
+printf("Fundamentals of ict\n");
+printf("Communication skills\n");
+printf("Analogue electronics\n");
+
+return 0;
+
