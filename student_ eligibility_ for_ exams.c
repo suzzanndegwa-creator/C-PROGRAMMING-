217853d895f492d70c3da378e2ff13a89bec7e12
suzzanndegwa-creator <suzzanndegwa@gmail.com>
2025-10-01 10:42:46 +0300

    Add files via upload

---
+++ student_ eligibility_ for_ exams.c
@@ -0,0 +1,28 @@
+//simple c program
+/*
+Name:Ndegwa Susan
+Reg No:CT101/G/26435/25
+Description:Student eligibility for exams
+*/
+
+int main(){
+
+   int attendance,average_marks;
+
+   printf("Enter your attendance:");
+   scanf("%d",&attendance);
+
+   printf("Enter your average_marks:");
+   scanf("%d",&average_marks);
+
+   //check eligibility
+   if(attendance >=75&average_marks>=40){
+   printf("you are eligible.");}
+
+    else{ printf("Not eligible.");
+    }
+
+  return 0;
+
+
+}
