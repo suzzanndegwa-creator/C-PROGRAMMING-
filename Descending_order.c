936e046845659f3e5a17732f39b8139e8701cf6c
suzzanndegwa-creator <suzzanndegwa@gmail.com>
2025-10-09 21:56:20 +0300

    Add files via upload

---
+++ Descending_order.c
@@ -0,0 +1,16 @@
+/*
+Name:Susan Ndegwa
+Reg No:CT101/G/26435/25
+Description:Descending order
+*/
+#include<stdio.h>
+
+int main(){
+
+    //for loop to print from 100 to 50
+    int i;
+     for (i =100;i >=50;i--){
+        printf("%d\n", i);
+        }
+    return 0;
+}
