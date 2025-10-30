dcad3fb303af39e5158808ab38964a6c8a78703f
suzzanndegwa-creator <suzzanndegwa@gmail.com>
2025-10-30 20:42:17 +0300

    Add files via upload

--- Revenue.c
+++ Revenue.c
@@ -1,22 +1,30 @@
 /*
-Name: Ndegwa Susan Mumbi
-Reg No:CT101/G/26435/25
-Description:The revenue
+Name:Susan Mumbi Ndegwa
+RegNo:CT101/G/26435/25
+Date:30/10/2025
+Description:A program for storing a 1D array revenue of a hotel for seven days
 */
-#include<stdio.h>
-   int main(){
-      int revenue[7];
-      float total_revenue=0;
-      float average_revenue;
+ #include<stdio.h>//preprocessor directive scanf(),printf()
 
-      printf("Enter the revenue for each of the 7 days:\n");
-       int i=0;i<7;i++;{
-      printf("Day%d:",i+1);
-      scanf("%d",&revenue[i]);
-      total_revenue=revenue[i];
-      }
-      average_revenue=total_revenue/7;
-      printf("Total revenue in a week:%.2f\n",total_revenue);
-      printf("Average revenue in a day:%.2f\n",average_revenue);
-      return 0;
-       }
+int main() {
+    int revenue[7];
+    float total_revenue = 0;
+    float average_revenue;
+
+    printf("Enter the revenue for each of the 7 days:\n");
+    for (int i = 0; i < 7; i++) {
+        printf("Day %d: ", i + 1);
+        scanf("%d", &revenue[i]);
+//calculate total weekly revenue
+        total_revenue += revenue[i];
+    }
+//calculate average daily revenue
+    average_revenue = total_revenue / 7;
+//output the total weeekly and average daily revenue
+
+    printf("\nTotal weekly revenue: %.2f\n", total_revenue);
+    printf("Average daily revenue: %.2f\n", average_revenue);
+
+    return 0;
+
+ }
