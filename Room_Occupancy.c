052fc20438e06437b1d8a2b428c598ce66c4dd32
suzzanndegwa-creator <suzzanndegwa@gmail.com>
2025-10-30 20:47:11 +0300

    Add files via upload

---
+++ Room_Occupancy.c
@@ -0,0 +1,41 @@
+/*
+Name:Susan Mumbi Ndegwa
+RegNo:CT101/G/26435/25
+Date:30/10/2025
+Description:A program for assigning and tracking the rooms occupied and number of vacant rooms
+*/
+
+
+
+#include <stdio.h>
+#include<stdio.h>
+#include<stdio.h>
+
+int main() {
+    int occupancy[5][10];
+    int occupied_rooms, vacant_rooms;
+    srand(time(NULL));
+
+    // Assign random occupancy
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 10; j++) {
+            occupancy[i][j] = rand() % 2; // 0 or 1
+        }
+    }
+
+    // Display occupancy per floor
+    for (int i = 0; i < 5; i++) {
+        occupied_rooms = 0;
+        vacant_rooms = 0;
+        for (int j = 0; j < 10; j++) {
+            if (occupancy[i][j] == 1) {
+                occupied_rooms++;
+            } else {
+                vacant_rooms++;
+            }
+        }
+        printf("Floor %d: %d occupied, %d vacant\n", i + 1, occupied_rooms, vacant_rooms);
+    }
+
+    return 0;
+}
